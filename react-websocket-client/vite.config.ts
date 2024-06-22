762e26f769a8da829dc1176a7b4ec3e7e7a2b289
Ivan Iakimenko <isirc10@gmail.com>
2024-06-22 12:08:07 -0400

    Add js rollup plugin visualizer, refactor main cpp startup functions

--- react-websocket-client/vite.config.ts
+++ react-websocket-client/vite.config.ts
@@ -1,9 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { visualizer } from 'rollup-plugin-visualizer'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    visualizer({
+      filename: 'dist/stats.html',
+      open: true,
+    }),
+  ],
   build: {
     emptyOutDir: true,
     outDir: '../data/www',
--- src/main.cpp
+++ src/main.cpp
@@ -25,34 +25,70 @@ void setup()
   pinMode(2, OUTPUT);
   Serial.begin(115200);
 
-  WiFi.disconnect();   // added to start with the wifi off, avoid crashing
-  WiFi.mode(WIFI_OFF); // added to start with the wifi off, avoid crashing
-  WiFi.mode(WIFI_AP);
-  WiFi.softAP(SSID);
+  initWifi();
+  configWifi();
+  startWifi();
+  initFileSystem();
+  startWebsocketServer();
+}
 
-  WiFi.softAPConfig(apIP, apIP, gateway);
-  dnsServer.start(DNS_PORT, "*", apIP);
-  Serial.println("\nWiFi AP is now running\nIP address: ");
-  Serial.println(WiFi.softAPIP());
-
-
-  if (!LittleFS.begin())
-  {
-    Serial.println("An Error has occurred while mounting LittleFS");
-    return;
-  }
-
+void startWebsocketServer()
+{
   // bind websocket to async web server
   websocket.onEvent(wsEventHandler);
   server.addHandler(&websocket);
-  // setup statuc web server
-  server.serveStatic("/", LittleFS, "/www/")
-      .setDefaultFile("index.html");
+  // setup static web server
+  server.serveStatic("/", LittleFS, "/www/").setDefaultFile("index.html");
   // Captive portal to keep the client
   server.onNotFound(redirectToIndex);
   server.begin();
   Serial.println("Server Started");
+}
 
+void startWifi()
+{
+  dnsServer.start(DNS_PORT, "*", apIP);
+  Serial.println("\nWiFi AP is now running\nIP address: ");
+  Serial.println(WiFi.softAPIP());
+}
+
+void initFileSystem()
+{
+  if (!LittleFS.begin())
+  {
+    Serial.println("An Error has occurred while mounting LittleFS");
+    while (true)
+    {
+      delay(1000); // Stop execution in case of FS error
+    }
+  }
+}
+
+void configWifi()
+{
+  if (!WiFi.softAPConfig(apIP, apIP, gateway))
+  {
+    Serial.println("AP Config Failed");
+    while (true)
+    {
+      delay(1000); // Stop execution if AP config fails
+    }
+  }
+}
+
+void initWifi()
+{
+  WiFi.disconnect();   // added to start with the wifi off, avoid crashing
+  WiFi.mode(WIFI_OFF); // added to start with the wifi off, avoid crashing
+  WiFi.mode(WIFI_AP);
+  if (!WiFi.softAP(SSID))
+  {
+    Serial.println("AP Start Failed");
+    while (true)
+    {
+      delay(1000); // Stop execution if AP start fails
+    }
+  }
 }
 
 void loop()
